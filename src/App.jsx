2a9c4d4f967707172f27139b63084bfa3bbb4b95
Konrad Pękala <kgpekala@gmail.com>
2024-09-29 11:55:20 +0200

    remove changing title

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Map from './components/map/Map';
 
 function App() {
-  document.title = '🚴‍♀️ CycleSafe';
   return (
     <BrowserRouter>
       <Routes>
