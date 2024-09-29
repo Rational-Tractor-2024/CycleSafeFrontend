44e5b6f76c9dad151cbc0750dbff931d02a7ce8b
Stanisław <Stanislaw.Darowski101@gmail.com>
2024-09-29 09:11:03 +0200

    wip

--- src/components/route/Route.jsx
+++ src/components/route/Route.jsx
@@ -2,46 +2,51 @@ import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
 import InputGroup from 'react-bootstrap/InputGroup';
 import Form from 'react-bootstrap/Form';
+import './Route.css';
 
 function Route({ source, target, setSource, setTarget, onSubmit }) {
-  return (
-    <Card style={{ width: '18rem', position: 'absolute', top: 72, left: 16 }}>
-      <Card.Body>
-        <Card.Title>Create a Route</Card.Title>
+	return (
+		<Card id='side-menu'>
+			<Card.Body>
+				<Card.Title>Create a Route</Card.Title>
 
-        <InputGroup className='mb-3'>
-          <InputGroup.Text id='basic-addon1'>A</InputGroup.Text>
-          <Form.Control
-            placeholder='From...'
-            value={source}
-            onChange={(e) => {
-              setSource(e.target.value);
-            }}
 
-            /* aria-label="Username"
-                aria-describedby="basic-addon1" */
-          />
-        </InputGroup>
+				<div className='form-box'>
 
-        <InputGroup className='mb-3'>
-          <InputGroup.Text id='basic-addon1'>B</InputGroup.Text>
-          <Form.Control
-            placeholder='To...'
-            value={target}
-            onChange={(e) => {
-              setTarget(e.target.value);
-            }}
-            /* aria-label="Username"
-                aria-describedby="basic-addon1" */
-          />
-        </InputGroup>
+					<div className='form-field'>
+						<div className="form-btn">A</div>
+						<Form.Control
+							placeholder='From...'
+							value={source}
+							onChange={(e) => {
+								setSource(e.target.value);
+							}}
 
-        <Button variant='primary' onClick={onSubmit}>
-          Go{' '}
-        </Button>
-      </Card.Body>
-    </Card>
-  );
+						/* aria-label="Username"
+							aria-describedby="basic-addon1" */
+						/>
+					</div>
+
+					<div className='form-field'>
+						<div className="form-btn">B</div>
+						<Form.Control
+							placeholder='To...'
+							value={target}
+							onChange={(e) => {
+								setTarget(e.target.value);
+							}}
+						/* aria-label="Username"
+							aria-describedby="basic-addon1" */
+						/>
+					</div>
+				</div>
+
+				<Button variant='primary' onClick={onSubmit}>
+					Go
+				</Button>
+			</Card.Body>
+		</Card>
+	);
 }
 
 export default Route;
