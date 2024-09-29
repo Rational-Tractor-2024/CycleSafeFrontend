bdc987573e88dfcd5317d61c414d138d04b52064
Konrad Pękala <kgpekala@gmail.com>
2024-09-29 11:42:01 +0200

    final commit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Map from './components/map/Map';
 
 function App() {
+  document.title = '🚴‍♀️ CycleSafe';
   return (
     <BrowserRouter>
       <Routes>
